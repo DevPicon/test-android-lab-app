17bc0c0081152e2d27959c108980620d58cbdafc
Armando Picón <picondev@gmail.com>
2018-08-22 20:04:10 -0300

    open Restore State Activity

--- app/src/main/java/pe/devpicon/android/mytestinglabapp/MainActivity.java
+++ app/src/main/java/pe/devpicon/android/mytestinglabapp/MainActivity.java
@@ -1,8 +1,12 @@
 package pe.devpicon.android.mytestinglabapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import pe.devpicon.android.mytestinglabapp.restorestate.RestoreStateActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void toRestoreState(View view) {
+        startActivity(new Intent(this, RestoreStateActivity.class));
+    }
 }
