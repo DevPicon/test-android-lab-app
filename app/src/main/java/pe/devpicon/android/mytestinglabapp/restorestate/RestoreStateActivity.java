ee76781176da480b4fb4a603971364bb8cb8e3f6
Armando Picón <picondev@gmail.com>
2018-08-22 20:01:14 -0300

    TEST #01 : Restore instance state with Fragments

---
+++ app/src/main/java/pe/devpicon/android/mytestinglabapp/restorestate/RestoreStateActivity.java
@@ -0,0 +1,70 @@
+package pe.devpicon.android.mytestinglabapp.restorestate;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.FrameLayout;
+
+import pe.devpicon.android.mytestinglabapp.R;
+
+import static pe.devpicon.android.mytestinglabapp.restorestate.RestoreStateFragment.EXTRA_KEY_DATA;
+import static pe.devpicon.android.mytestinglabapp.restorestate.RestoreStateFragment.EXTRA_KEY_DESCRIPTION;
+
+public class RestoreStateActivity extends AppCompatActivity {
+    private static final String TAG = "RestoreStateActivity";
+    private static final String FRAGMENT_SAVED_STATE = "fragment_saved_state";
+    private static final String FRAGMENT_TAG = "fragment_tag";
+    private FrameLayout frameLayoutFragmentContainer;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restore_state);
+
+        frameLayoutFragmentContainer = findViewById(R.id.fragment_contrainer);
+
+        loadFragment();
+    }
+
+    private void loadFragment() {
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+        RestoreStateFragment restoreStateFragment = new RestoreStateFragment();
+        Bundle extraBundle = new Bundle();
+        extraBundle.putInt(EXTRA_KEY_DATA, 123);
+        extraBundle.putString(EXTRA_KEY_DESCRIPTION, "This is a initial description");
+        fragmentTransaction.replace(R.id.fragment_contrainer, restoreStateFragment, FRAGMENT_TAG);
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState() called with: outState = [" + outState + "]");
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        Fragment fragmentByTag = supportFragmentManager.findFragmentByTag(FRAGMENT_TAG);
+        Fragment.SavedState savedState = supportFragmentManager.saveFragmentInstanceState(fragmentByTag);
+        outState.putParcelable(FRAGMENT_SAVED_STATE, savedState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState() called with: savedInstanceState = [" + savedInstanceState + "]");
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(FRAGMENT_SAVED_STATE)) {
+                Fragment.SavedState savedState = savedInstanceState.getParcelable(FRAGMENT_SAVED_STATE);
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                Fragment fragmentByTag = getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
+                if (fragmentByTag instanceof RestoreStateFragment) {
+                    fragmentByTag.setInitialSavedState(savedState);
+                }
+                fragmentTransaction.commit();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/pe/devpicon/android/mytestinglabapp/restorestate/RestoreStateFragment.java
@@ -0,0 +1,68 @@
+package pe.devpicon.android.mytestinglabapp.restorestate;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import pe.devpicon.android.mytestinglabapp.R;
+
+public class RestoreStateFragment extends Fragment {
+    private static final String TAG = "RestoreStateFragment";
+    public static final String EXTRA_KEY_DATA = "key_data";
+    public static final String EXTRA_KEY_DESCRIPTION = "key_data_description";
+
+    private EditText editTextDataId, editTextDataDescription;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_restore_state, container, false);
+        editTextDataId = rootView.findViewById(R.id.edit_text_data_id);
+        editTextDataDescription = rootView.findViewById(R.id.edit_text_data_description);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            editTextDataId.setText(String.valueOf(arguments.getInt(EXTRA_KEY_DATA)));
+            editTextDataDescription.setText(arguments.getString(EXTRA_KEY_DESCRIPTION));
+        }
+
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState() called with: outState = [" + outState + "]");
+        int value = (TextUtils.isEmpty(editTextDataId.getText().toString())) ? 0 : Integer.parseInt(editTextDataId.getText().toString());
+        String description = (TextUtils.isEmpty(editTextDataDescription.getText())) ? "Empty" : editTextDataDescription.getText().toString();
+        outState.putInt(EXTRA_KEY_DATA, value);
+        outState.putString(EXTRA_KEY_DESCRIPTION, description);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onViewStateRestored() called with: savedInstanceState = [" + savedInstanceState + "]");
+        super.onViewStateRestored(savedInstanceState);
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(EXTRA_KEY_DATA)) {
+                int anInt = savedInstanceState.getInt(EXTRA_KEY_DATA);
+                String anString = savedInstanceState.getString(EXTRA_KEY_DESCRIPTION);
+
+                editTextDataId.setText(String.valueOf(anInt));
+                editTextDataDescription.setText(anString);
+            }
+        }
+    }
+}
