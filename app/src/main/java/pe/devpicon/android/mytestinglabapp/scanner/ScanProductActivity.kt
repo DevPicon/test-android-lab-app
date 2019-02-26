cd4ae4075efdc44d58ea04562e37424932d87b27
Armando Picón <picondev@gmail.com>
2019-02-26 12:35:15 -0300

    init commit scanner

---
+++ app/src/main/java/pe/devpicon/android/mytestinglabapp/scanner/ScanProductActivity.kt
@@ -0,0 +1,13 @@
+package pe.devpicon.android.mytestinglabapp.scanner
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pe.devpicon.android.mytestinglabapp.R
+
+class ScanProductActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan_product)
+    }
+}
