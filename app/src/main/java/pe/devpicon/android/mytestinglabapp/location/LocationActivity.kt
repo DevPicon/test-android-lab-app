fcc5d5ff505dc79431210604f4ce2f567f9594f2
Armando Picón <picondev@gmail.com>
2019-06-05 17:32:09 -0400

    location base implementation

---
+++ app/src/main/java/pe/devpicon/android/mytestinglabapp/location/LocationActivity.kt
@@ -0,0 +1,13 @@
+package pe.devpicon.android.mytestinglabapp.location
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pe.devpicon.android.mytestinglabapp.R
+
+class LocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+    }
+}
